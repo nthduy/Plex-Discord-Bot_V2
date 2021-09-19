1933cf22cada6ece8c2ff76f3c170d83a782b7b9
Duy NGUYEN <nth.duy1511@gmail.com>
2021-09-19 16:13:53 +0200

    Syntax fixes, reformat code

--- commands/song.js
+++ commands/song.js
@@ -1,17 +1,15 @@
 module.exports = {
-  name : 'song',
-  command : {
-    usage: '',
-    description: 'displays current song',
-    process: function(bot, client, message) {
-      let messageLines = '';
-      if (bot.songQueue.length > 0) {
-        let embedObj = bot.songToEmbedObject(bot.songQueue[0]);
-        message.channel.send(bot.language.BOT_PLAYSONG_SUCCES, embedObj);
-      }
-      else {
-        message.reply(bot.language.VIEWQUEUE_FAIL);
-      }
+    name: 'song',
+    command: {
+        usage: '',
+        description: 'displays current song',
+        process: function (bot, client, message) {
+            if (bot.songQueue.length > 0) {
+                let embedObj = bot.songToEmbedObject(bot.songQueue[0]);
+                message.channel.send(bot.language.BOT_PLAYSONG_SUCCES, embedObj);
+            } else {
+                message.reply(bot.language.VIEWQUEUE_FAIL);
+            }
+        }
     }
-  }
 };
