6d6aec0e23285a6b057f5fe061e482e6702aa9e8
Tu le <tule@deevotech.com>
2021-09-21 23:37:54 +0700

    Now Random have query number

--- commands/random.js
+++ commands/random.js
@@ -1,18 +1,18 @@
 module.exports = {
-  name : 'random',
-  command : {
-    usage: '',
-    description: 'bot will join voice channel and play a random song.',
-    process: async function(bot, client, message, query) {
+  name: "random",
+  command: {
+    usage: "<number or it will be generate only 1 song>",
+    description: "bot will join voice channel and play a random song.",
+    process: async function (bot, client, message, query) {
       if (query.length === 0) {
         try {
-          await bot.findRandomTracksOnPlex(message);
-        } catch (err){
+          await bot.findRandomTracksOnPlex(query, message);
+        } catch (err) {
           console.log(err);
         }
       } else {
-        message.reply('random don\'t take argument.');
+        message.reply("random don't take argument.");
       }
-    }
-  }
+    },
+  },
 };
